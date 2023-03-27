c4b736f08dc2de936b6e9537f03d41b70e33afcf
AisakaPMD <amelie.ambletonguth@gmail.com>
2023-03-27 17:32:36 +0200

    added map, player.py and a tilemap utility

---
+++ src/player.py
@@ -0,0 +1,23 @@
+import struct
+import constants
+
+farm_map_size = (0, 0)
+farm_map = None
+entities = None
+user = None
+
+def load_map():
+	fname = "assets/maps/farm_map.bin"
+	mapf = open(fname, "rb")
+	mapWidth = struct.unpack("I", mapf.read(4))[0]
+	mapHeight = struct.unpack("I", mapf.read(4))[0]
+	farm_map = []
+	for y in range(mapHeight):
+		farm_map.append([])
+		for x in range(mapHeight):
+			farm_map[y].append(struct.unpack("c", mapf.read(1))[0])
+	farm_map_size = (mapWidth, mapHeight)
+
+
+def save():
+	pass
